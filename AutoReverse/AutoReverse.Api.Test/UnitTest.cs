5b87e7e4aa98f9ad943338cf18d976a7f595b487
superbr4in <15275491+superbr4in@users.noreply.github.com>
2018-03-04 15:17:23 +0100

    Created simple test methods.

---
+++ AutoReverse/AutoReverse.Api.Test/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutoReverse.Api.Test
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            FooClass.Foo();
+        }
+    }
+}
---
+++ AutoReverse/AutoReverse.Api/FooClass.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AutoReverse.Api
+{
+    public class FooClass
+    {
+        [DllImport("AutoReverse.LibWrapper.dll")]
+        private static extern void foo();
+
+        public static void Foo()
+        {
+            foo();
+        }
+    }
+}
