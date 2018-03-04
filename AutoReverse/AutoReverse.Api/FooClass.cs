27e7850a959894ae771d4dcb1122f5442c21878b
superbr4in <15275491+superbr4in@users.noreply.github.com>
2018-03-04 18:34:59 +0100

    Added `disassembler` struct with seeking behavior.

--- AutoReverse/AutoReverse.Api/FooClass.cs
+++ AutoReverse/AutoReverse.Api/FooClass.cs
@@ -1,43 +1,64 @@
 ﻿using System;
+using System.Linq;
 using System.Runtime.InteropServices;
 
 namespace AutoReverse.Api
 {
     public static class FooClass
     {
-        private struct FooStruct
+        private struct Instruction
         {
-            public uint id;
+            public uint Id;
 
-            public ulong address;
+            public ulong Address;
 
-            public ushort size;
+            public ushort Size;
+
             [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16, ArraySubType = UnmanagedType.U8)]
-            public byte[] bytes;
+            public byte[] Bytes;
 
             [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
-            public string mnemonic;
+            public string Mnemonic;
 
             [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 160)]
-            public string opStr;
-
-            public IntPtr detail;
+            public string OpStr;
         }
 
         [DllImport("AutoReverse.LibWrapper.dll")]
-        private static extern int disasm(byte[] bytes, ref FooStruct s);
+        private static extern IntPtr create_disassembler(byte[] bytes);
+
+        [DllImport("AutoReverse.LibWrapper.dll")]
+        private static extern void seek(ulong offset);
+
+        [DllImport("AutoReverse.LibWrapper.dll")]
+        private static extern int disassemble(IntPtr disassembler, out Instruction instruction);
 
         public static int Foo()
         {
-            var s = new FooStruct();
+            var dis = create_disassembler(new byte[] { 0x55, 0x48, 0x8b, 0x05, 0xb8, 0x13, 0x00, 0x00 });
 
-            var result = disasm(
-                new byte[] { 0x55, 0x48, 0x8b, 0x05, 0xb8, 0x13, 0x00, 0x00 }, ref s);
+            for (var i = 0; i < 2; i++)
+            {
+                var result = disassemble(dis, out var instr);
 
-            if (result == 0)
-                Console.WriteLine(s.mnemonic);
+                if (result != 0)
+                    return result;
+                
+                var bytesStr = $"{instr.Bytes[0]:X2}";
+                for (var j = 1; j < instr.Bytes.Length; j++)
+                {
+                    var b = instr.Bytes[j];
 
-            return result;
+                    if (b == 0xCD)
+                        break;
+
+                    bytesStr += $" {b:X2}";
+                }
+
+                Console.WriteLine($"{instr.Address:X8} :\t{instr.Mnemonic} {instr.OpStr}\t({bytesStr})");
+            }
+
+            return 0;
         }
     }
 }
