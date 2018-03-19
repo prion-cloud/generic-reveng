95096e38a7ca17f1e400cb247baba06cc1535fd1
superbr4in <15275491+superbr4in@users.noreply.github.com>
2018-03-19 17:56:15 +0100

    Created simple GUI.

--- SharpReverse/SharpReverse/MainWindow.xaml.cs
+++ SharpReverse/SharpReverse/MainWindow.xaml.cs
@@ -1,10 +1,51 @@
-﻿namespace SharpReverse
+﻿using System.ComponentModel;
+using System.Windows;
+
+using Microsoft.Win32;
+
+using SharpReverse.Api;
+
+namespace SharpReverse
 {
     public partial class MainWindow
     {
+        private Debugger _debugger;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _debugger = null;
+        }
+
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            _debugger?.Dispose();
+        }
+
+        private void Button_Load_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog
+            {
+                Filter = "All files|*.*"
+            };
+
+            if (!dialog.ShowDialog(this).Value)
+                return;
+
+            TextBlock.Text = string.Empty;
+
+            _debugger = new Debugger(dialog.FileName);
+        }
+
+        private void Button_Step_Click(object sender, RoutedEventArgs e)
+        {
+            if (_debugger == null)
+                return;
+
+            var debug = _debugger.Debug32();
+
+            TextBlock.Text += $"0x{debug.Address:x8} {debug.Instruction}\n";
         }
     }
 }
