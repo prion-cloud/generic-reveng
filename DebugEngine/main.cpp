ef76c32859e42d720b6e0c5ecab4e3399adf07c1
superbr4in <15275491+superbr4in@users.noreply.github.com>
2018-05-14 00:27:50 +0200

    Updated highlighting for special instructions.

--- DebugEngine/main.cpp
+++ DebugEngine/main.cpp
@@ -127,10 +127,35 @@ void print_trace_entry(const debug_trace_entry trace_entry)
     std::cout << "\t";
 
     auto col = COL_DEF;
-    if (instruction.id == 0x10a)
+    switch (instruction.id)
+    {
+    case X86_INS_JMP:
+    case X86_INS_JO:
+    case X86_INS_JNO:
+    case X86_INS_JS:
+    case X86_INS_JNS:
+    case X86_INS_JE:
+    case X86_INS_JNE:
+    case X86_INS_JB:
+    case X86_INS_JAE:
+    case X86_INS_JBE:
+    case X86_INS_JA:
+    case X86_INS_JL:
+    case X86_INS_JGE:
+    case X86_INS_JLE:
+    case X86_INS_JG:
+    case X86_INS_JP:
+    case X86_INS_JNP:
+    case X86_INS_JCXZ:
         col = COL_JUMP;
-    if (instruction.id == 0x038 || instruction.id == 0x095)
+        break;
+    case X86_INS_CALL:
+    case X86_INS_RET:
         col = COL_CALL;
+        break;
+    default:;
+    }
+
     COUT_COL(col, << instruction.mnemonic << " " << instruction.operands);
 
     if (!label.empty())
