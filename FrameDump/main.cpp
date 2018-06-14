468fbb7db04ba0c0aa6576fbbdf24f3a8c06a244
superbr4in <15275491+superbr4in@users.noreply.github.com>
2018-06-14 18:24:06 +0200

    More C for a better performance.

--- FrameDump/main.cpp
+++ FrameDump/main.cpp
@@ -2,9 +2,6 @@
 
 #include "../Bin-Capstone/capstone.h"
 
-#include "instruction.h"
-#include "serialization.h"
-
 static size_t get_size(std::ifstream& stream)
 {
     const auto pos = stream.tellg();
@@ -17,68 +14,32 @@ static size_t get_size(std::ifstream& stream)
     return size;
 }
 
-static std::vector<x86_instruction>* disassemble_all(const uint64_t address, const std::vector<uint8_t>* bytes)
+static std::vector<cs_insn> disassemble_all(const uint64_t address, const std::vector<uint8_t> bytes)
 {
-    std::cout << "Disassembling...";
-
     csh handle;
     cs_open(CS_ARCH_X86, CS_MODE_64, &handle);
     cs_option(handle, CS_OPT_DETAIL, CS_OPT_ON);
     cs_option(handle, CS_OPT_SKIPDATA, CS_OPT_ON);
 
-    cs_insn* cs_instructions;
-    const auto count = cs_disasm(handle, &bytes->at(0), bytes->size(), address, 0, &cs_instructions);
+    cs_insn* c_disassembly;
+    const auto count = cs_disasm(handle, &bytes.at(0), bytes.size(), address, 0, &c_disassembly);
 
     cs_close(&handle);
 
-    std::cout << "Converting...";
+    const std::vector<cs_insn> disassembly(c_disassembly, c_disassembly + count);
 
-    auto disassembly = new std::vector<x86_instruction>;
-    for (auto i = 0; i < 0; ++i)
-    {
-        const auto cs_ins = cs_instructions[i];
-
-        x86_instruction ins;
-
-        ins.id = static_cast<x86_insn>(cs_ins.id);
-        ins.address = cs_ins.address;
-        ins.bytes = std::vector<uint8_t>(cs_ins.bytes, cs_ins.bytes + cs_ins.size);
-        ins.representation = std::string(cs_ins.mnemonic) + " " + std::string(cs_ins.op_str);
-
-        if (cs_ins.detail == nullptr)
-            continue;
-
-        const auto cs_ins_detail = cs_ins.detail->x86;
-
-        for (auto j = 0; j < cs_ins_detail.op_count; ++j)
-        {
-            const auto cs_op = cs_ins_detail.operands[j];
-
-            x86_operator op;
-
-            op.type = cs_op.type;
-            op.value = cs_op.imm;
-
-            ins.operators.push_back(op);
-        }
-
-        disassembly->push_back(ins);
-    }
-
-    cs_free(cs_instructions, count);
-
-    std::cout << "Finished" << std::endl;
+    cs_free(c_disassembly, count);
 
     return disassembly;
 }
 
-static std::map<uint64_t, size_t> find_sequences(std::vector<x86_instruction>* disassembly, const int min, const unsigned find, const std::set<unsigned> add)
+static std::map<uint64_t, size_t> find_sequences(std::vector<cs_insn> disassembly, const int min, const unsigned find, const std::set<unsigned> add)
 {
     std::map<uint64_t, size_t> sequences;
 
-    for (auto i = 0; i < disassembly->size(); ++i)
+    for (auto i = 0; i < disassembly.size(); ++i)
     {
-        const auto ins = disassembly->at(i);
+        const auto ins = disassembly.at(i);
 
         if (ins.id != find)
             continue;
@@ -90,10 +51,10 @@ static std::map<uint64_t, size_t> find_sequences(std::vector<x86_instruction>* d
             ++i;
             ++j;
 
-            if (i >= disassembly->size())
+            if (i >= disassembly.size())
                 break;
         }
-        while (disassembly->at(i).id == find || add.find(disassembly->at(i).id) != add.end());
+        while (disassembly.at(i).id == find || add.find(disassembly.at(i).id) != add.end());
 
         if (j >= min)
         {
@@ -105,48 +66,26 @@ static std::map<uint64_t, size_t> find_sequences(std::vector<x86_instruction>* d
     return sequences;
 }
 
-static void save_disassembly(const std::string file_name, const std::vector<x86_instruction>* disassembly)
+static void save_disassembly(const std::string file_name, const std::vector<cs_insn> disassembly)
 {
-    std::cout << "Saving...";
-
-    std::ofstream file_stream(file_name, std::ios::binary);
-
-    file_stream <<= static_cast<uint64_t>(disassembly->size());
-
-    for (const auto ins : *disassembly)
-        file_stream <<= ins;
-
-    std::cout << "Finished" << std::endl;
+    std::ofstream(file_name, std::ios::binary).write(reinterpret_cast<const char*>(&disassembly.at(0)), disassembly.size() * sizeof(cs_insn));
 }
-static std::vector<x86_instruction>* load_disassembly(const std::string file_name)
+static std::vector<cs_insn> load_disassembly(const std::string file_name)
 {
     std::ifstream file_stream(file_name, std::ios::binary);
 
-    uint64_t size = 0;
-    file_stream >>= size;
+    const auto size = get_size(file_stream);
 
-    auto disassembly = new std::vector<x86_instruction>;
-    for (auto i = 0; i < size; ++i)
-    {
-        x86_instruction ins;
-        file_stream >>= ins;
-        disassembly->push_back(ins);
-    }
+    const auto c_disassembly = new char[size];
+    file_stream.read(c_disassembly, size);
+
+    const std::vector<cs_insn> disassembly(
+        reinterpret_cast<cs_insn*>(c_disassembly),
+        reinterpret_cast<cs_insn*>(c_disassembly) + size / sizeof(cs_insn));
+
+    delete[] c_disassembly;
 
     return disassembly;
-}
-
-static std::vector<uint8_t>* load_bytes(const std::string file_name)
-{
-    std::ifstream file_stream(file_name, std::ios::binary);
-
-    if (!file_stream.is_open())
-        return nullptr;
-
-    auto bytes = new std::vector<uint8_t>(get_size(file_stream));
-    file_stream.read(reinterpret_cast<char*>(&bytes->at(0)), bytes->size());
-
-    return bytes;
 }
 
 int main(const int argc, char* argv[])
@@ -160,48 +99,35 @@ int main(const int argc, char* argv[])
     const std::string file_name(argv[1]);
 
     std::cout << "File: \"" << file_name << "\"" << std::endl;
+    
+    std::ifstream file_stream(file_name, std::ios::binary);
 
-    const auto bytes = load_bytes(file_name);
-
-    if (bytes == nullptr)
+    if (!file_stream.is_open())
     {
         std::cerr << "Could not open file." << std::endl;
         return -1;
     }
 
-    std::cout << "Size: " << bytes->size() << " bytes" << std::endl;
+    std::vector<uint8_t> bytes(get_size(file_stream));
+    file_stream.read(reinterpret_cast<char*>(&bytes.at(0)), bytes.size());
+
+    std::cout << "Size: " << bytes.size() << " bytes" << std::endl;
 
     // -----
 
     const uint64_t addr1 = 0x1000;
     const uint64_t addr2 = 0x989000;
 
-    //std::vector<uint8_t> text;
-    //text.insert(text.end(), b_text1.begin(), b_text1.end());
-    //text.insert(text.end(), b_text2.begin(), b_text2.end());
-
-    const auto b_text1 = new std::vector<uint8_t>(bytes->begin() + addr1, bytes->begin() + 0x4b5a00);
+    const std::vector<uint8_t> b_text1(bytes.begin() + addr1, bytes.begin() + 0x4b5a00);
+    const std::vector<uint8_t> b_text2(bytes.begin() + addr2, bytes.begin() + 0xe66000);
 
     const auto ins1 = disassemble_all(addr1, b_text1);
-
-    delete b_text1;
-    delete bytes;
+    const auto ins2 = disassemble_all(addr2, b_text2);
 
     save_disassembly("ins1.dis", ins1);
-
-    delete ins1;
-
-    //const auto b_text2 = new std::vector<uint8_t>(bytes.begin() + addr2, bytes.begin() + 0xe66000);
-    //const auto ins2 = disassemble_all(addr2, b_text2);
-    //delete b_text2;
-    //save_disassembly("ins2.dis", ins2);
-    //delete ins2;
-
-    //const auto seq = find_sequences(ins2, 10, X86_INS_POP, { X86_INS_PUSHFQ, X86_INS_MOVUPD, X86_INS_LEA });
+    save_disassembly("ins2.dis", ins2);
 
     std::cout << "Complete" << std::endl;
-
-    //FATAL_IF(ins1 != load);
 
     // -----
 
