54e12799bfab565b6e8d99edaf866afd3e5ab328
superbr4in <15275491+superbr4in@users.noreply.github.com>
2018-07-08 23:50:07 +0200

    Guiding thread

--- PatternGenerator/main.cpp
+++ PatternGenerator/main.cpp
@@ -1,10 +1,134 @@
 #include "stdafx.h"
 
-#include "display.h"
+#include "control_flow.h"
+#include "data_flow.h"
+#include "disassembly.h"
+#include "frame_detector.h"
+#include "rule.h"
 
-int main()
+static std::vector<uint64_t> addresses
+{
+    0x7FF7E4E2DD4C,
+/*
+    0x7FF7E4E2EBDA,
+    0x7FF7E4E43F63,
+    0x7FF7E4E50722,
+    0x7FF7E4E79064,
+    0x7FF7E4F3EEB9,
+    0x7FF7E4F4438A,
+    0x7FF7E4F58D0E,
+    0x7FF7E4F7F9BF,
+    0x7FF7E4FA8225,
+    0x7FF7E4FE3F3A,
+    0x7FF7E5002462,
+    0x7FF7E5006F17,
+    0x7FF7E5021D8C,
+    0x7FF7E502A93A,
+    0x7FF7E50AF887,
+    0x7FF7E50FFB14,
+    0x7FF7E50FFE8B
+*/
+};
+
+static std::vector<uint8_t> load(const std::string& file_name)
+{
+    std::cout << "File: \"" << file_name << "\"" << std::endl;
+
+    std::ifstream file_stream(file_name, std::ios::binary);
+
+    if (!file_stream.is_open())
+        return { };
+
+    file_stream.seekg(0, std::ios::end);
+    std::vector<uint8_t> code(file_stream.tellg());
+
+    file_stream.seekg(0, std::ios::beg);
+
+    file_stream.read(reinterpret_cast<char*>(&code.at(0)), code.size());
+
+    std::cout << "Size: " << code.size() << " bytes" << std::endl;
+
+    return code;
+}
+
+int main(const int argc, char* argv[])
 {
     dsp::h_console = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    return 0;
+    if (argc != 2)
+    {
+        std::cerr << dsp::colorize(FOREGROUND_RED) << "Invalid number of arguments."
+                  << dsp::decolorize << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    const auto code = load(argv[1]);
+
+    if (code.empty())
+    {
+        std::cerr << dsp::colorize(FOREGROUND_RED) << "Could not open file."
+                  << dsp::decolorize << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::cout << std::endl;
+
+/* ---------------------------------------------------------------- */
+
+    const disassembly disassembly(code);
+
+    frame_detector detector(disassembly);
+
+    std::vector<instruction_sequence> pool;
+    for (const auto address : addresses)
+    {
+        detector.seek(address);
+
+        uint64_t start, stop;
+        if (!detector.next(start, stop))
+        {
+            std::cerr << dsp::colorize(FOREGROUND_RED) << "No more frames found."
+                      << dsp::decolorize << std::endl;
+            return EXIT_FAILURE;
+        }
+
+        const control_flow control_flow(disassembly, start, stop);
+        control_flow.draw();
+
+        const auto blocks = control_flow.get_blocks();
+        pool.insert(pool.end(), blocks.begin(), blocks.end());
+    }
+
+    std::map<instruction_sequence, unsigned> power_pool;
+    for (const auto& seq : pool)
+    {
+        for (const auto& subseq : seq.power())
+            ++power_pool[subseq];
+    }
+
+    std::map<data_flow, std::vector<instruction_sequence>> flow_map;
+    for (const auto& [seq, count] : power_pool)
+    {
+        const data_flow data_flow(seq);
+        if (!data_flow->empty())
+            flow_map[data_flow].push_back(seq);
+    }
+
+    deobfuscation_rule_set rule_set;
+    for (const auto& [data_flow, seqs] : flow_map)
+    {
+        for (const auto& seq : seqs)
+        {
+            // TODO: MAGIC
+        }
+    }
+
+    rule_set.json_serialize("pattern_database.json");
+
+/* ---------------------------------------------------------------- */
+
+    std::cout << std::endl << "COMPLETE" << std::endl;
+    std::cin.get();
+
+    return EXIT_SUCCESS;
 }
