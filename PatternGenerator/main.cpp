c539efa57ce4d07e897094ae50accb04dd0d5ac6
superbr4in <15275491+superbr4in@users.noreply.github.com>
2018-07-09 20:33:31 +0200

    Replaced frame_detector with array of start/stops.

--- PatternGenerator/main.cpp
+++ PatternGenerator/main.cpp
@@ -3,31 +3,28 @@
 #include "control_flow.h"
 #include "data_flow.h"
 #include "disassembly.h"
-#include "frame_detector.h"
 #include "rule_set.h"
 
-static std::vector<uint64_t> addresses
+static std::vector<std::pair<uint64_t, uint64_t>> addresses
 {
-    0x7FF7E4E2DD4C,
-/*
-    0x7FF7E4E2EBDA,
-    0x7FF7E4E43F63,
-    0x7FF7E4E50722,
-    0x7FF7E4E79064,
-    0x7FF7E4F3EEB9,
-    0x7FF7E4F4438A,
-    0x7FF7E4F58D0E,
-    0x7FF7E4F7F9BF,
-    0x7FF7E4FA8225,
-    0x7FF7E4FE3F3A,
-    0x7FF7E5002462,
-    0x7FF7E5006F17,
-    0x7FF7E5021D8C,
-    0x7FF7E502A93A,
-    0x7FF7E50AF887,
-    0x7FF7E50FFB14,
-    0x7FF7E50FFE8B
-*/
+    { 0x7FF7E4E2DD4C, 0x7FF7E4FF67A0 },
+    { 0x7FF7E4E2EBDA, 0x7FF7E4E7CAC0 },
+    { 0x7FF7E4E43F63, 0x7FF7E4F5DC10 },
+    { 0x7FF7E4E50722, 0x7FF7E50AD001 },
+    { 0x7FF7E4E79064, 0x7FF7E4F5E830 },
+    { 0x7FF7E4F3EEB9, 0x7FF7E4EBC500 },
+    { 0x7FF7E4F4438A, 0x7FF7E4F09DA7 },
+    { 0x7FF7E4F58D0E, 0x7FF7E4F3C969 },
+    { 0x7FF7E4F7F9BF, 0x7FF7E4F44C97 },
+    { 0x7FF7E4FA8225, 0x7FF7E4E2DEF8 },
+    { 0x7FF7E4FE3F3A, 0x7FF7E4EB9C72 },
+    { 0x7FF7E5002462, 0x7FF7E4FF6182 },
+    { 0x7FF7E5006F17, 0x7FF7E4E83659 },
+    { 0x7FF7E5021D8C, 0x7FF7E4E89D84 },
+    { 0x7FF7E502A93A, 0x7FF7E4EF33C4 },
+    { 0x7FF7E50AF887, 0x7FF7E4EEBE4E },
+    { 0x7FF7E50FFB14, 0x7FF7E50FC3AC },
+    { 0x7FF7E50FFE8B, 0x7FF7E4FF73E4 }
 };
 
 static std::vector<uint8_t> load(const std::string& file_name)
@@ -77,21 +74,9 @@ int main(const int argc, char* argv[])
 
     const disassembly disassembly(code);
 
-    frame_detector detector(disassembly);
-
     std::vector<instruction_sequence> pool;
-    for (const auto address : addresses)
+    for (const auto [start, stop] : addresses)
     {
-        detector.seek(address);
-
-        uint64_t start, stop;
-        if (!detector.next(start, stop))
-        {
-            std::cerr << dsp::colorize(FOREGROUND_RED) << "No more frames found."
-                      << dsp::decolorize << std::endl;
-            return EXIT_FAILURE;
-        }
-
         const control_flow control_flow(disassembly, start, stop);
         control_flow.draw();
 
