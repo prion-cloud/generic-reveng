b9db3207869c6651ff5819803a6611f827d40c22
superbr4in <15275491+superbr4in@users.noreply.github.com>
2018-07-25 13:12:54 +0200

    Added catch2 test structure.

---
+++ test/test.cpp
@@ -0,0 +1,10 @@
+#define CATCH_CONFIG_RUNNER
+#include "../catch2/single_include/catch2/catch.hpp"
+
+int main(const int argc, char* argv[])
+{
+    Catch::Session().run(argc, argv);
+
+    system("pause");
+    return 0;
+}
