0d73ed97cd2e61af014666c95ac1eea0ae472f81
superbr4in <15275491+superbr4in@users.noreply.github.com>
2019-01-06 12:56:54 +0100

    Implemented CFG block positioning.

--- scout-cli/ControlFlowGraph.cs
+++ scout-cli/ControlFlowGraph.cs
@@ -43,28 +43,75 @@ public partial class ControlFlowGraph : IDisposable
 
     public void Show(ITextDisplay display)
     {
-        var instructionStrings =
-            _root.Instructions
-                .Select(ins => ins.ToString())
-                .ToArray();
+        var positionedBlocks = PositionBlocks(_root);
 
-        var canvas = new TextCanvas();
-        canvas[0].Add(
-            new TextRectangle(0, 0, instructionStrings)
+        var xSpaces = new int[positionedBlocks.Values.Max(v => v.X) + 1];
+        var ySpaces = new int[positionedBlocks.Values.Max(v => v.Y) + 1];
+
+        var visualizedBlocks =
+            positionedBlocks
+                .ToDictionary(
+                    block => block.Value,
+                    block =>
+                        (TextShape)
+                            new TextRectangle(
+                                block.Value,
+                                block.Key.Instructions
+                                    .Select(instruction => instruction.ToString())
+                                    .ToArray())
+                                {
+                                    TopLeft  = DR,
+                                    TopRight = DL,
+
+                                    BottomLeft  = UR,
+                                    BottomRight = UL,
+
+                                    HLine = H,
+                                    VLine = V
+                                });
+
+        foreach (var block in visualizedBlocks.Values)
+        {
+            if (xSpaces[block.Position.X] < block.Size.X)
+                xSpaces[block.Position.X] = block.Size.X;
+            if (ySpaces[block.Position.Y] < block.Size.Y)
+                ySpaces[block.Position.Y] = block.Size.Y;
+        }
+
+        var position = Vector.Zero;
+        for (var y = 0; y < ySpaces.Length; y++)
+        {
+            var ySpace = ySpaces[y];
+
+            for (var x = 0; x < xSpaces.Length; x++)
             {
-                TopLeft  = DR,
-                TopRight = DL,
+                var xSpace = xSpaces[x];
 
-                BottomLeft  = UR,
-                BottomRight = UL,
+                TextShape block;
+                if (visualizedBlocks.TryGetValue(new Vector(x, y), out block))
+                {
+                    block.Position =
+                        position +
+                        new Vector(
+                            xSpace / 2 - block.Size.X / 2,
+                            ySpace / 2 - block.Size.Y / 2);
+                }
 
-                HLine = H,
-                VLine = V
-            });
+                position.X += xSpace;
+            }
 
-        /* TODO */
+            position.X = 0;
+            position.Y += ySpace;
+        }
+
+        var canvas = new TextCanvas
+        {
+            { 0, visualizedBlocks.Values }
+        };
 
         display.Content = canvas.Illustrate();
+
+        /* TODO */
 
         var loop = true;
         do
@@ -97,6 +144,62 @@ public partial class ControlFlowGraph : IDisposable
     {
         CfgDestruct(_handle);
     }
+
+    private static Dictionary<Block, Vector> PositionBlocks(Block root)
+    {
+        var positionedBlocks = new Dictionary<Block, Vector>
+        {
+            { root, Vector.Zero }
+        };
+        var visited = new HashSet<Block>();
+
+        PositionBlocks(root, ref positionedBlocks, ref visited);
+
+        return positionedBlocks;
+    }
+    private static void PositionBlocks(
+        Block parent,
+        ref Dictionary<Block, Vector> positionedBlocks,
+        ref HashSet<Block> visited)
+    {
+        visited.Add(parent);
+
+        var parentPosition = positionedBlocks[parent];
+
+        var uniqueChildIndex = 0;
+        foreach (var child in parent.Children)
+        {
+            if (visited.Contains(child))
+                continue;
+
+            var newChildPosition =
+                new Vector(
+                    parentPosition.X + uniqueChildIndex,
+                    parentPosition.Y + 1);
+
+            Vector existingChildPosition;
+            if (positionedBlocks.TryGetValue(child, out existingChildPosition))
+            {
+                if (existingChildPosition.Y > parentPosition.Y)
+                    continue;
+
+                positionedBlocks[child] =
+                    new Vector(
+                        existingChildPosition.X,
+                        newChildPosition.Y);
+            }
+            else
+            {
+                positionedBlocks.Add(child, newChildPosition);
+
+                uniqueChildIndex++;
+            }
+
+            PositionBlocks(child, ref positionedBlocks, ref visited);
+        }
+
+        visited.Remove(parent);
+    }
 }
 
 public partial class Block
@@ -108,7 +211,7 @@ public partial class Block
         _handle = handle;
     }
 
-    public IEnumerable<Block> Successors
+    public IEnumerable<Block> Children
     {
         get
         {
@@ -131,6 +234,20 @@ public partial class Block
                 yield return instruction;
             }
         }
+    }
+
+    public override bool Equals(object obj)
+    {
+        var other = obj as Block;
+        if (other == null)
+            return false;
+
+        return other._handle == _handle;
+    }
+
+    public override int GetHashCode()
+    {
+        return _handle.GetHashCode();
     }
 }
 
