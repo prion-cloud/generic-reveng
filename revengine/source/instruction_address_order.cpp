8664db16189e2c102e5da389494e70ca7bf0c9c5
superbr4in <15275491+superbr4in@users.noreply.github.com>
2019-10-31 01:20:06 +0100

    Separated ordering

---
+++ revengine/source/instruction_address_order.cpp
@@ -0,0 +1,26 @@
+#include <revengine/instruction.hpp>
+
+namespace rev
+{
+    bool instruction::address_order::operator()(instruction const& instruction_1, instruction const& instruction_2) const
+    {
+        return instruction_1.address() < instruction_2.address();
+    }
+
+    bool instruction::address_order::operator()(instruction const& instruction, std::uint64_t const address) const
+    {
+        return instruction.address() < address;
+    }
+    bool instruction::address_order::operator()(std::uint64_t const address, instruction const& instruction) const
+    {
+        return address < instruction.address();
+    }
+
+    static_assert(std::is_destructible_v<instruction::address_order>);
+
+    static_assert(std::is_move_constructible_v<instruction::address_order>);
+    static_assert(std::is_move_assignable_v<instruction::address_order>);
+
+    static_assert(std::is_copy_constructible_v<instruction::address_order>);
+    static_assert(std::is_copy_assignable_v<instruction::address_order>);
+}
--- revengine/source/instruction_block.cpp
+++ revengine/source/instruction_block.cpp
@@ -2,34 +2,6 @@
 
 namespace rev
 {
-    bool instruction_block::exclusive_address_order::operator()(
-        instruction_block const& instruction_block_1,
-        instruction_block const& instruction_block_2) const
-    {
-        return instruction_block_1.rbegin()->address() < instruction_block_2.address();
-    }
-
-    bool instruction_block::exclusive_address_order::operator()(
-        instruction_block const& instruction_block,
-        std::uint64_t const address) const
-    {
-        return instruction_block.rbegin()->address() < address;
-    }
-    bool instruction_block::exclusive_address_order::operator()(
-        std::uint64_t const address,
-        instruction_block const& instruction_block) const
-    {
-        return address < instruction_block.address();
-    }
-
-    static_assert(std::is_destructible_v<instruction_block::exclusive_address_order>);
-
-    static_assert(std::is_move_constructible_v<instruction_block::exclusive_address_order>);
-    static_assert(std::is_move_assignable_v<instruction_block::exclusive_address_order>);
-
-    static_assert(std::is_copy_constructible_v<instruction_block::exclusive_address_order>);
-    static_assert(std::is_copy_assignable_v<instruction_block::exclusive_address_order>);
-
     instruction_block::instruction_block() = default;
 
     std::uint64_t instruction_block::address() const
@@ -37,7 +9,7 @@ namespace rev
         return begin()->address();
     }
 
-    // TODO
+    // TODO (?)
     machine_impact instruction_block::impact() const
     {
         machine_impact impact;
